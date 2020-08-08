b7a3f7d2fe44195173260a08a612d900b1398f2f
Petr Horacek <phoracek@redhat.com>
2020-08-08 21:42:54 +0200

    Turn Rack into a generic type
    With this change, Rack and Unit become generic types. That means that
    they are not limited to i32 anymore, but they can carry an arbitrary
    type. A new test mutating fields within struct was added to illustrate
    that.
    
    Signed-off-by: Petr Horacek <phoracek@redhat.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,17 @@ use core::ops::{Deref, DerefMut};
 use core::ptr;
 
 #[derive(Debug)]
-pub struct Rack {
+pub struct Rack<T> {
     // All the stored units are kept inside `RefCell` to allow us to keep a
     // mutable reference to the data in multiple `Unit`s while keeping the
     // `Rack` immutable. That way we avoid issues with borrow checking.
     // The carried type is then enclosed in `MaybeUnit`, the reason for that we
     // don't need to require carried type to implement `Copy` and `Default` to
     // populate the whole array during `Rack`'s initialization.
-    data: [RefCell<MaybeUninit<i32>>; 2],
+    data: [RefCell<MaybeUninit<T>>; 2],
 }
 
-impl Rack {
+impl<T> Rack<T> {
     pub fn new() -> Self {
         Self {
             // TODO: Use "Constants in array repeat expressions" once it is
@@ -30,7 +30,7 @@ impl Rack {
         }
     }
 
-    pub fn add(&self, value: i32) -> Unit {
+    pub fn add(&self, value: T) -> Unit<T> {
         for cell in self.data.iter() {
             // If we can borrow it, nobody has a mutable reference, it is free
             // to take.
@@ -45,25 +45,25 @@ impl Rack {
     }
 }
 
-impl Default for Rack {
+impl<T> Default for Rack<T> {
     fn default() -> Self {
         Self::new()
     }
 }
 
 #[derive(Debug)]
-pub struct Unit<'a> {
-    cell: RefMut<'a, MaybeUninit<i32>>,
+pub struct Unit<'a, T> {
+    cell: RefMut<'a, MaybeUninit<T>>,
 }
 
-impl Unit<'_> {
-    pub fn get_ref(&self) -> &i32 {
+impl<T> Unit<'_, T> {
+    pub fn get_ref(&self) -> &T {
         // This code is safe since we always populate the `MaybeUninit` with a
         // value on `add` call before an `Unit` is returned.
         unsafe { &*self.cell.as_ptr() }
     }
 
-    pub fn get_mut(&mut self) -> &mut i32 {
+    pub fn get_mut(&mut self) -> &mut T {
         // This code is safe since we always populate the `MaybeUninit` with a
         // value on `add` call before an `Unit` is returned.
         unsafe { &mut *self.cell.as_mut_ptr() }
@@ -73,7 +73,7 @@ impl Unit<'_> {
 // The payload is carried inside `MaybeUninit`. `Drop` on `MaybeUninit` does not
 // do anything. Therefore, we have to implement the `Drop` trait, making sure
 // that a destructor is called on the carried payload.
-impl Drop for Unit<'_> {
+impl<T> Drop for Unit<'_, T> {
     fn drop(&mut self) {
         unsafe {
             ptr::drop_in_place(self.cell.as_mut_ptr());
@@ -81,15 +81,15 @@ impl Drop for Unit<'_> {
     }
 }
 
-impl Deref for Unit<'_> {
-    type Target = i32;
+impl<T> Deref for Unit<'_, T> {
+    type Target = T;
 
     fn deref(&self) -> &Self::Target {
         self.get_ref()
     }
 }
 
-impl DerefMut for Unit<'_> {
+impl<T> DerefMut for Unit<'_, T> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         self.get_mut()
     }
@@ -101,21 +101,21 @@ mod tests {
 
     #[test]
     fn initialize_rack() {
-        let _rack: Rack = Rack::new();
+        let _rack: Rack<_> = Rack::<i32>::new();
     }
 
     #[test]
     fn add_unit_to_rack() {
-        let rack = Rack::new();
+        let rack = Rack::<i32>::new();
 
-        let _unit: Unit = rack.add(10);
+        let _unit: Unit<_> = rack.add(10);
     }
 
     #[test]
     fn get_immutable_reference_to_unit_value() {
         let rack = Rack::new();
 
-        let unit: Unit = rack.add(10);
+        let unit = rack.add(10);
 
         assert_eq!(*unit.get_ref(), 10);
     }
@@ -124,7 +124,7 @@ mod tests {
     fn get_multiple_immutable_references_to_unit_value() {
         let rack = Rack::new();
 
-        let unit: Unit = rack.add(10);
+        let unit = rack.add(10);
 
         let ref_1 = unit.get_ref();
         let ref_2 = unit.get_ref();
@@ -136,7 +136,7 @@ mod tests {
     fn get_mutable_reference_to_unit_value() {
         let rack = Rack::new();
 
-        let mut unit: Unit = rack.add(10);
+        let mut unit = rack.add(10);
 
         assert_eq!(*unit.get_mut(), 10);
     }
@@ -145,7 +145,7 @@ mod tests {
     fn access_unit_value_by_dereferencing() {
         let rack = Rack::new();
 
-        let unit: Unit = rack.add(10);
+        let unit = rack.add(10);
 
         assert_eq!(*unit, 10);
     }
@@ -158,7 +158,7 @@ mod tests {
 
         let rack = Rack::new();
 
-        let unit: Unit = rack.add(10);
+        let unit = rack.add(10);
 
         assert_ref_i32_eq_10(&unit)
     }
@@ -167,7 +167,7 @@ mod tests {
     fn change_unit_value_through_mutable_reference() {
         let rack = Rack::new();
 
-        let mut unit: Unit = rack.add(10);
+        let mut unit = rack.add(10);
 
         let mut_ref = unit.get_mut();
         *mut_ref = 20;
@@ -176,10 +176,24 @@ mod tests {
     }
 
     #[test]
+    fn change_unit_struct_field_through_mutable_reference() {
+        struct Foo(i32);
+
+        let rack = Rack::new();
+
+        let mut unit = rack.add(Foo(10));
+
+        let mut_ref = unit.get_mut();
+        mut_ref.0 = 20;
+
+        assert_eq!(unit.get_ref().0, 20);
+    }
+
+    #[test]
     fn change_unit_value_by_mutable_dereferencing() {
         let rack = Rack::new();
 
-        let mut unit: Unit = rack.add(10);
+        let mut unit = rack.add(10);
         *unit = 20;
 
         assert_eq!(*unit.get_ref(), 20);
@@ -194,7 +208,7 @@ mod tests {
 
         let rack = Rack::new();
 
-        let mut unit: Unit = rack.add(10);
+        let mut unit = rack.add(10);
 
         assert_mut_ref_i32_editable(&mut unit)
     }
@@ -203,8 +217,8 @@ mod tests {
     fn accept_up_to_the_limit() {
         let rack = Rack::new();
 
-        let _unit1: Unit = rack.add(10);
-        let _unit2: Unit = rack.add(20);
+        let _unit1 = rack.add(10);
+        let _unit2 = rack.add(20);
     }
 
     #[test]
@@ -212,19 +226,19 @@ mod tests {
     fn reject_over_the_limit() {
         let rack = Rack::new();
 
-        let _unit1: Unit = rack.add(10);
-        let _unit2: Unit = rack.add(20);
-        let _unit3: Unit = rack.add(30);
+        let _unit1 = rack.add(10);
+        let _unit2 = rack.add(20);
+        let _unit3 = rack.add(30);
     }
 
     #[test]
     fn accept_more_units_once_old_ones_get_out_of_scope() {
         let rack = Rack::new();
 
-        let _unit1: Unit = rack.add(10);
+        let _unit1 = rack.add(10);
         {
-            let _unit2: Unit = rack.add(20);
+            let _unit2 = rack.add(20);
         }
-        let _unit3: Unit = rack.add(30);
+        let _unit3 = rack.add(30);
     }
 }
